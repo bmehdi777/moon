e5d4df51309b42a62627988ca9f7ba64202833a9
bmehdi777 <mehdi.bentouati76@gmail.com>
2024-11-22 00:25:16 +0100

    feat: add ovh provider

--- internal/pkg/server/dns_providers/ovh.go
+++ internal/pkg/server/dns_providers/ovh.go
@@ -1,1 +1,32 @@
 package dns_providers
+
+import (
+	"log"
+
+	"github.com/bmehdi777/moon/internal/pkg/server/config"
+	"github.com/ovh/go-ovh/ovh"
+)
+
+type OVHProvider struct {
+	client *ovh.Client
+}
+
+func NewClient() *OVHProvider {
+	var provider OVHProvider
+
+	client, err := ovh.NewClient(
+		config.GlobalConfig.OvhProvider.Endpoint,
+		config.GlobalConfig.OvhProvider.ApplicationKey,
+		config.GlobalConfig.OvhProvider.ApplicationSecret,
+		config.GlobalConfig.OvhProvider.ConsumerKey,
+	)
+	if err != nil {
+		log.Fatalf("Can't initialize OVH client : %v", err)
+	}
+
+	provider = OVHProvider{
+		client: client,
+	}
+
+	return &provider
+}
---
+++ internal/pkg/server/dns_providers/provider.go
@@ -0,0 +1,3 @@
+package dns_providers
+
+type DNSProvider interface {}
