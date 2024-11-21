1d69c64afa0d63e2c499cbe1764eec4be95f069d
bmehdi777 <mehdi.bentouati76@gmail.com>
2024-11-21 23:30:45 +0100

    feat: fix config gitignore

---
+++ internal/pkg/server/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+
+type Config struct {
+	CertKeyPath string `mapstructure:"cert_key"`
+	CertPemPath string `mapstructure:"cert_pub"`
+
+	HttpAddr string `mapstructure:"http_addr"`
+	HttpPort string `mapstructure:"http_port"`
+	TcpAddr string `mapstructure:"server_addr"`
+	TcpPort string `mapstructure:"server_port"`
+
+	DatabasePath string `mapstructure:"database_path"`
+}
+
+var GlobalConfig Config
+
+func InitConfig() {
+	GlobalConfig = Config{
+		CertKeyPath: "./certs/server.key",
+		CertPemPath: "./certs/server.pem",
+		HttpAddr: "0.0.0.0",
+		HttpPort: "8080",
+		TcpAddr: "0.0.0.0",
+		TcpPort: "4040",
+		DatabasePath: "./moon.db",
+	}
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("/etc/moon/")
+	viper.AddConfigPath("$HOME/.config/moon/")
+	viper.AddConfigPath(".")
+	err := viper.Unmarshal(&GlobalConfig)
+	if err != nil {
+		log.Fatalf("Unable to unmarshal config file, %v", err)
+	}
+}
