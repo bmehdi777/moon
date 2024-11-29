6a3e9bcfbc8435317f304a2944ffc2c160445a49
bmehdi777 <mehdi.bentouati76@gmail.com>
2024-11-29 01:09:59 +0100

    feat: add get token

--- internal/pkg/agent/cmd/login.go
+++ internal/pkg/agent/cmd/login.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/url"
@@ -11,13 +12,15 @@ import (
 	"os/exec"
 	"runtime"
 	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/oauth2"
 )
 
+// to put in a parameter
 const BASE_URL_KEYCLOAK = "http://localhost:8080"
 
-// http://localhost:8080/realms/moon/protocol/openid-connect/auth?client_id=moon-test&redirect_uri=https%3A%2F%2Fwww.keycloak.org%2Fapp%2F%23url%3Dhttp%3A%2F%2Flocalhost%3A8080%26realm%3Dmoon%26client%3Dmoon-test&state=0b23d3a6-b15f-4e6c-bd29-06b96bed1fd3&response_mode=fragment&response_type=code&scope=openid
 func NewCmdLogin() *cobra.Command {
 	loginCmd := cobra.Command{
 		Use:   "login",
@@ -30,20 +33,27 @@ func NewCmdLogin() *cobra.Command {
 }
 
 func handlerLogin(cmd *cobra.Command, args []string) {
-	authCode, err := getAuthorizationCode()
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		fmt.Println("Can't open server : ", err)
+	}
+	port := strconv.Itoa(listener.Addr().(*net.TCPAddr).Port)
+
+	codeVerifier := oauth2.GenerateVerifier()
+	codeChallenge := oauth2.S256ChallengeFromVerifier(codeVerifier)
+
+	authCode, err := getAuthorizationCode(listener, port, codeChallenge)
 	if err != nil {
 		fmt.Println("An error occured : ", err)
 	}
 
 	fmt.Println("Authorization code : ", authCode)
+	bodyResponse := getToken(authCode, codeVerifier, "http://127.0.0.1:"+port)
+
+	fmt.Println("\nBody response : ", bodyResponse)
 }
 
-func getAuthorizationCode() (string, error) {
-	listener, err := net.Listen("tcp", "127.0.0.1:0")
-	if err != nil {
-		return "", err
-	}
-
+func getAuthorizationCode(listener net.Listener, port string, challenge string) (string, error) {
 	mux := http.NewServeMux()
 	srv := http.Server{
 		Handler: mux,
@@ -52,23 +62,57 @@ func getAuthorizationCode() (string, error) {
 	var authCode string
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Successfully logged in."))
 		authCode = r.URL.Query().Get("code")
+		if authCode != "" {
+			w.Write([]byte("Successfully logged in."))
+		} else {
+			w.Write([]byte("An error occured while trying to log you. Try later."))
+		}
 		go srv.Shutdown(context.Background())
 	})
 
-	port := listener.Addr().(*net.TCPAddr).Port
-	loginUri := createLoginUri(strconv.Itoa(port))
+	loginUri := createLoginUri(challenge, port)
 	fmt.Println("If your browser isn't open, you can click on the following link : \n\n", loginUri)
 
 	openInBrowser(loginUri)
 
-	err = srv.Serve(listener)
+	err := srv.Serve(listener)
 	if err != nil && err != http.ErrServerClosed {
 		return "", err
 	}
 
 	return authCode, nil
+}
+
+func getToken(authCode string, verifier string, callbackUri string) string {
+	encodedRedirectUri := url.QueryEscape(callbackUri)
+	url := BASE_URL_KEYCLOAK + "/realms/moon/protocol/openid-connect/token"
+
+	var payloadString strings.Builder
+	payloadString.WriteString("grant_type=authorization_code")
+	payloadString.WriteString("&client_id=moon-agent")
+	payloadString.WriteString("&code=" + authCode)
+	payloadString.WriteString("&code_verifier=" + verifier)
+	payloadString.WriteString("&redirect_uri=" + encodedRedirectUri)
+
+	fmt.Println("DEBUG : ", payloadString.String())
+	payload := strings.NewReader(payloadString.String())
+
+	req, _ := http.NewRequest("POST", url, payload)
+	req.Header.Add("content-type", "application/x-www-form-urlencoded")
+
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	return string(body)
+}
+
+func createLoginUri(challenge string, port string) string {
+	redirectUri := "http://127.0.0.1:" + port
+	encodedRedirectUri := url.QueryEscape(redirectUri)
+
+	return BASE_URL_KEYCLOAK + "/realms/moon/protocol/openid-connect/auth?client_id=moon-agent&redirect_uri=" + encodedRedirectUri + "&response_type=code&scope=openid+email&code_challenge_method=S256&code_challenge=" + challenge
 }
 
 func openInBrowser(url string) error {
@@ -96,11 +140,4 @@ func openInBrowser(url string) error {
 	} else {
 		return errors.New("Unsupported platform")
 	}
-}
-
-func createLoginUri(port string) string {
-	redirectUri := "http://127.0.0.1:" + port
-	encodedRedirectUri := url.QueryEscape(redirectUri)
-	return BASE_URL_KEYCLOAK + "/realms/moon/protocol/openid-connect/auth?client_id=moon-agent&redirect_uri=" + encodedRedirectUri + "&response_type=code&scope=openid+email"
-
 }
