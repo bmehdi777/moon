84bf7f484e313b2b1070800907d9b46ea672ab70
bmehdi777 <mehdi.bentouati76@gmail.com>
2025-01-12 21:35:06 +0100

    move to assets folder

--- scripts/makecert.sh
+++ scripts/makecert.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-rm certs/*
 echo "make server cert"
-openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=$1"
+openssl req -new -nodes -x509 -out assets/server/certs/server.pem -keyout assets/server/certs/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=m00n/OU=IT/CN=m00n.fr/emailAddress=$1"
 echo "make client cert"
-openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=$1"
+openssl req -new -nodes -x509 -out assets/client/certs/client.pem -keyout assets/client/certs/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=$1"
