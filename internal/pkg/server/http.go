83895d5d7d00f9df751d41442144bca99ce5b924
bmehdi777 <mehdi.bentouati76@gmail.com>
2025-01-19 23:30:37 +0100

    feat: handle api request - wip

--- internal/pkg/server/http.go
+++ internal/pkg/server/http.go
@@ -6,23 +6,47 @@ import (
 	"log"
 	"net/http"
 
+	"gorm.io/gorm"
 	"moon/internal/pkg/server/config"
 	"moon/internal/pkg/server/database"
-	"gorm.io/gorm"
 )
 
 func httpServe(channelsPerDomain *ChannelsDomains, db *gorm.DB) error {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		handleAllRequest(w, r, channelsPerDomain, db)
+	mux := http.NewServeMux()
+
+	tunHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handleTunnelRequest(w, r, channelsPerDomain, db)
 	})
 
-	fullAddrFmt := fmt.Sprintf("%v:%v", config.GlobalConfig.App.TcpAddr, config.GlobalConfig.App.HttpPort)
+	mux.Handle("/api/healthcheck", middlewareApiTun(apiHealthcheck, tunHandler))
+	mux.HandleFunc("/", tunHandler)
+
+	fullAddrFmt := fmt.Sprintf("%v:%v", config.GlobalConfig.App.HttpAddr, config.GlobalConfig.App.HttpPort)
 	log.Printf("HTTP server is up at %v", fullAddrFmt)
-	err := http.ListenAndServe(fullAddrFmt, nil)
+	err := http.ListenAndServe(fullAddrFmt, mux)
 	return err
 }
 
-func handleAllRequest(w http.ResponseWriter, r *http.Request, channelsPerDomain *ChannelsDomains, db *gorm.DB) {
+func middlewareApiTun(api http.HandlerFunc, tun http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO: not working currently, see why
+		// when I request subdomain.domain.fr/random, it waits indefinitely
+		// request is for api
+		fmt.Println("url : ", r.Host)
+		fmt.Println("moon url : ", config.GlobalConfig.App.GlobalDomainName)
+		if r.Host == config.GlobalConfig.App.GlobalDomainName {
+			api.ServeHTTP(w, r)
+		} else {
+			tun.ServeHTTP(w, r)
+		}
+	})
+}
+
+func apiHealthcheck(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
+func handleTunnelRequest(w http.ResponseWriter, r *http.Request, channelsPerDomain *ChannelsDomains, db *gorm.DB) {
 	// have to ensure request are going to the right chan
 	// so we need to process the domain name
 	hostRequest := r.Host
