cd6bf8ac575921f4669902849b04e1c8f4b1d8f4
bmehdi777 <mehdi.bentouati76@gmail.com>
2025-04-20 15:40:46 +0200

    feat: change how routing is made

--- internal/pkg/server/api/api.go
+++ internal/pkg/server/api/api.go
@@ -2,17 +2,18 @@ package api
 
 import (
 	"net/http"
+	"strconv"
 	"strings"
 )
 
 type App struct {
-	HealthcheckHandler Healthcheck
+	HealthcheckHandler  Healthcheck
 	CertificatesHandler Certificates
 }
 
 func NewApp() App {
 	return App{
-		HealthcheckHandler: Healthcheck{},
+		HealthcheckHandler:  Healthcheck{},
 		CertificatesHandler: Certificates{},
 	}
 }
@@ -20,12 +21,50 @@ func NewApp() App {
 func (a *App) ServeHttp(w http.ResponseWriter, r *http.Request) {
 	path := strings.Replace(r.URL.Path, "/api", "", 1)
 
-	switch path {
-	case "/healthcheck":
-		a.HealthcheckHandler.ServeHttp(w, r)
-	case "/certificates":
-		a.CertificatesHandler.ServeHttp(w, r)
+	var certsId string
+
+	switch {
+	case match(path, "/health"):
+		a.HealthcheckHandler.handleGet(w)
+	case match(path, "/certificates") && r.Method == "POST":
+		a.CertificatesHandler.handleCreate(w)
+	case match(path, "/certificates/+", &certsId) && r.Method == "GET":
+		a.CertificatesHandler.handleGet(certsId, w, r)
 	default:
 		http.Error(w, "Not found", http.StatusNotFound)
 	}
+}
+
+func match(path, pattern string, vars ...interface{}) bool {
+	for ; pattern != "" && path != ""; pattern = pattern[1:] {
+		switch pattern[0] {
+		case '+':
+			// '+' matches till next slash in path
+			slash := strings.IndexByte(path, '/')
+			if slash < 0 {
+				slash = len(path)
+			}
+			segment := path[:slash]
+			path = path[slash:]
+			switch p := vars[0].(type) {
+			case *string:
+				*p = segment
+			case *int:
+				n, err := strconv.Atoi(segment)
+				if err != nil || n < 0 {
+					return false
+				}
+				*p = n
+			default:
+				panic("vars must be *string or *int")
+			}
+			vars = vars[1:]
+		case path[0]:
+			// non-'+' pattern byte must match path byte
+			path = path[1:]
+		default:
+			return false
+		}
+	}
+	return path == "" && pattern == ""
 }
--- internal/pkg/server/api/certificates.go
+++ internal/pkg/server/api/certificates.go
@@ -1,18 +1,15 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type Certificates struct{}
 
-func (c *Certificates) ServeHttp(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		c.handlePost(r, w)
-	default:
-		w.WriteHeader(http.StatusNotFound)
-	}
+func (c *Certificates) handleCreate( w http.ResponseWriter) {
+	w.WriteHeader(http.StatusOK)
 }
 
-func (c *Certificates) handlePost(r *http.Request, w http.ResponseWriter) {
+func (c *Certificates) handleGet(id string, w http.ResponseWriter, r *http.Request) {
 
 }
